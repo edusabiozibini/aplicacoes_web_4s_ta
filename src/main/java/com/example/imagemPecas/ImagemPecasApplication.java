3d5b13ea45a82162a09cfd9dcb56bb866bd84573
Eduardo <eduardosabio33@gmail.com>
2026-09-02 22:04:43 -0300

    aula 05 fazendo o post para enviar as imagens

--- src/main/java/com/example/imagemPecas/ImagemPecasApplication.java
+++ src/main/java/com/example/imagemPecas/ImagemPecasApplication.java
@@ -1,5 +1,6 @@
 package com.example.imagemPecas;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +9,20 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class ImagemPecasApplication {
 
+    /*
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired ImageRepository repository){
+        return args ->{
+            Image image = Image.builder()
+                    .extension(ImageExtension.PNG)
+                    .name("myimage")
+                    .tags("teste")
+                    .size(1000L)
+                    .build();
+            repository.save(image);
+        };
+    }
+*/
 	public static void main(String[] args) {
 		SpringApplication.run(ImagemPecasApplication.class, args);
 	}
